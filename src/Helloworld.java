555dfd57911844980f8bb2d49b24229b560914e0
KISHOREKUMAR-GCE <kishoreramya112@gmail.com>
2024-10-07 01:30:27 +0530

    helloworld

---
+++ src/Helloworld.java
@@ -0,0 +1,10 @@
+public class Helloworld
+{
+    public static void main(String args[])
+    {
+        System.out.println("HELLO JAVA");
+        System.out.print("hi");
+        System.out.print("welcome");
+        System.out.println("welcome narmadha");
+    }
+}
